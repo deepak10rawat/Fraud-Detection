2cf47886b1f9b0519741b69e9b130a0564ed3cee
deepak10rawat <deepak.rawat1009@gmail.com>
2022-01-29 21:33:40 +0530

    adding fot sake of testin

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,15 @@
+from tabnanny import check
+
+
 print ('Adding all the data processing to fraud detection')
+a = 4
+b = 5
+def sum(a,b):
+    c=a+b
+    return c
+
+def mul(a,b):
+    d=a*b
+    return d
+
+    Print (Hi this is check)
